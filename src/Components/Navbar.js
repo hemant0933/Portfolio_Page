ddccdc12e9616a6a63d342738094d47066fc6e68
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-05 18:53:15 +0530

    removed state in nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,39 +1,9 @@
 import { Link } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 
 const Navbar = () => {
-  // const [theme, setTheme] = useState({
-  //   color: "black",
-  //   backgroundColor: "white",
-  // });
-
-  // const toggleBtn = () => {
-  //   if (theme.color === "black") {
-  //     setTheme({
-  //       color: "white",
-  //       backgroundColor: "black",
-  //     });
-  //   } else {
-  //     setTheme({
-  //       color: "black",
-  //       backgroundColor: "white",
-  //     });
-  //   }
-  // };
 
   return (
-    // <div className='nav-wrapper' style={theme}>
-    //   <h1 className='nav-left'>PortFolio <strong className='dot'>.</strong></h1>
-    //   <div className='nav-right'>
-    //     <span className='nav_list'>
-    //         <Link href='#about'><p className='headernav'>About</p></Link>
-    //         <Link href='#skills'><p className='headernav'>Skills</p></Link>
-    //         <Link href='#projects'><p className='headernav'>Projects</p></Link>
-    //         <Link href='#socials'><p className='headernav'>Socials</p></Link>
-    //         <Link><p className='headernav' onClick={toggleBtn} value={theme.color}> <HiOutlineLightBulb /> </p></Link>
-    //     </span>
-    //   </div>
-    // </div>
     <nav className="navbar navbar-expand-lg nav-wrapper fixed-top bg-body-tertiary">
       <div className="container-fluid">
         <button
@@ -60,9 +30,7 @@ const Navbar = () => {
               <h1>
                 PortFolio<strong className="dot">.</strong>
               </h1>
-            </Link>
-        
-         
+            </Link> 
             <ul className="navbar-nav ms-auto mb-lg-0">
               <li className="nav-item">
                 <Link
