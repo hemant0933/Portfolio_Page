490e5de4b3be6771ec5c012af2c5c5fee8d8f2e5
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-07-05 20:17:03 +0530

    merge conflict resolve

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Myvideo from '../Assets/tree.png';
 import swiggy from '../Assets/swiggy-clone.png';
@@ -8,32 +7,15 @@ import tic from '../Assets/tic-ta-toe.png' ;
 import DialogProjects from './DialogProjects';
 import connect from '../Assets/connect.png';
 import meshop from '../Assets/meshop.png';
-
+import Skills from './Skills'
+import Footer from './Footer'
 import { BsInstagram, BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
 import HerosSection from './HerosSection';
-=======
-import React from "react";
-
-import men from "../Assets/men.jpg";
-
-import swiggy from "../Assets/swiggy-clone.png";
-import flipkart from "../Assets/flipkart-clone.png";
-import pexel from "../Assets/pexel.png";
-import tic from "../Assets/tic-ta-toe.png";
-import DialogProjects from "./DialogProjects";
-import connect from "../Assets/connect.png";
-import meshop from "../Assets/meshop.png";
-
-import Intro from "./Intro";
-import Skills from "./Skills";
-import Footer from "./Footer";
->>>>>>> 20cafe2882f2c721c3dbff5791a5a37380e8ffc5
 
 const Main = () => {
   
   let projectData = [
-<<<<<<< HEAD
       {
         id:1,
         src:'https://hemant0933.github.io/swiggy-clone/',
@@ -66,9 +48,6 @@ const Main = () => {
       }
   ]
 
-
- 
-
   return (
     <div className='main-wrapper'>
        <div className='container main-inner-wrapper' id='about'>
@@ -81,56 +60,6 @@ const Main = () => {
            <img src={Myvideo} alt="tree" className='img-fluid'/>
           </div>
        </div>
-=======
-    {
-      id: 1,
-      src: "https://hemant0933.github.io/swiggy-clone/",
-      Imgsrc: swiggy,
-      title:'Swiggy Clone'
-    },
-    {
-      id: 2,
-      src: "https://hemant0933.github.io/Flipkart-Clone/",
-      Imgsrc: flipkart,
-      title:'Flipkart Clone'
-    },
-    {
-      id: 3,
-      src: "https://main--funny-bunny-ecd8ca.netlify.app/",
-      Imgsrc: pexel,
-      title:'Image searcher'
-    },
-    {
-      id: 4,
-      src: "https://hemant0933.github.io/Tik-Tac-Toe-game/",
-      Imgsrc: tic,
-      title:'Tic Tac Toe game'
-    },
-    {
-      id: 5,
-      src: "https://hemant0933.github.io/Bootstarpe-landing-page/",
-      Imgsrc: connect,
-      title:'Connect landing page'
-    },
-    {
-      id: 6,
-      src: "https://hemant0933.github.io/F3-Project/",
-      Imgsrc: meshop,
-      title:'MeShop Shop Webapp'
-    },
-  ];
-
-  return (
-    <div className="main-wrapper">
-      <div className="container main-inner-wrapper" id="about">
-        <Intro men={men} />
-        <div class="custom-shape-divider-bottom-1686061293">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-      </svg>
-    </div>
-      </div>
->>>>>>> 20cafe2882f2c721c3dbff5791a5a37380e8ffc5
 
      
       {/* Skills */}
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
-=======
-import { Link } from "@mui/material";
-import React from "react";
->>>>>>> 20cafe2882f2c721c3dbff5791a5a37380e8ffc5
 
 const Navbar = () => {
   const [scroll, setScroll] = useState(false);
   const [activeItem, setActiveItem] = useState(null);
 
-<<<<<<< HEAD
   const handleClick = (item) => {
     setActiveItem(item)
   }
@@ -68,69 +62,6 @@ const Navbar = () => {
         </ul>
       </div>
     </header>
-=======
-  return (
-    <nav className="navbar navbar-expand-lg nav-wrapper fixed-top bg-body-tertiary">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo01"
-          aria-controls="navbarTogglerDemo01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse"
-          id="navbarTogglerDemo01"
-        >
-         
-            <Link
-              className="navbar-brand"
-              style={{ textDecoration: "none",color:'#000'}}
-              href="/"
-            >
-              <h1>
-                PortFolio<strong className="dot">.</strong>
-              </h1>
-            </Link> 
-            <ul className="navbar-nav ms-auto mb-lg-0">
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  style={{ textDecoration: "none", color: "#000" }}
-                  aria-current="page"
-                  href="#about"
-                >
-                  <p className="headernav">About</p>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  style={{ textDecoration: "none", color: "#000" }}
-                  href="#skills"
-                >
-                  <p className="headernav">Skills</p>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  style={{ textDecoration: "none", color: "#000" }}
-                  href="#projects"
-                >
-                  <p className="headernav">Projects</p>
-                </Link>
-              </li>
-            </ul>
-          </div>
-      </div>
-    </nav>
->>>>>>> 20cafe2882f2c721c3dbff5791a5a37380e8ffc5
   );
 };
 
